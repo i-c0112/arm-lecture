6438dd9f37bc92193f97054fe60c22990ce96e04
i-c0112 <a7807118@gmail.com>
2014-08-06 17:03:39 +0800

    lab1, 2 lab1, 2

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -9,7 +9,22 @@
 	.type lock_mutex, function
 lock_mutex:
         @ INSERT CODE BELOW
+	// initialized once and for all
+	mov r1, #locked
 
+lock_retry:
+	ldrex r2, [r0]
+
+	cmp r2, r1
+	// retry, currently flag set to locked
+	beq lock_retry
+	// otherwise, change public flag to locked
+	strexne r2, r1, [r0] 
+
+	cmpne r2, #0 
+	// failed to strex, status != 0, retry
+	bne lock_retry
+	dmb
         @ END CODE INSERT
 	bx lr
 
@@ -19,7 +34,9 @@ lock_mutex:
 	.type unlock_mutex, function
 unlock_mutex:
 	@ INSERT CODE BELOW
-        
+        mov r1, #unlocked
+	dmb
+	str r1, [r0]
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
